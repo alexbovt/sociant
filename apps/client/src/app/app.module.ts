0fc380a14ea56d4176590c2dd684102aacbf96fb
alexbovt <bovttt@gmail.com>
2021-01-17 23:48:03 +0100

    add material ui

--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { TranslocoRootModule } from './transloco/transloco-root.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, TranslocoRootModule],
+  imports: [BrowserModule, HttpClientModule, TranslocoRootModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
